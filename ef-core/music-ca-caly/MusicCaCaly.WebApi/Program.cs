9d162577390ed0afee8b2e277ceb11cabbe88b55
Arnar Leifsson <aleifsson@deloitte.is>
2023-08-30 22:05:16 +0000

    Migrating Music-ca-caly to .NET 7

--- ef-core/music-ca-caly/MusicCaCaly.WebApi/Program.cs
+++ ef-core/music-ca-caly/MusicCaCaly.WebApi/Program.cs
@@ -1,26 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MusicCaCaly.Repositories.Contexts;
+using MusicCaCaly.Repositories.Implementations;
+using MusicCaCaly.Repositories.Interfaces;
+using MusicCaCaly.Services.Implementations;
+using MusicCaCaly.Services.Interfaces;
+using MusicCaCaly.WebApi.Converters;
 
-namespace MusicCaCaly.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddTransient<IAlbumService, AlbumService>();
+builder.Services.AddTransient<IArtistService, ArtistService>();
+builder.Services.AddTransient<IReviewService, ReviewService>();
+builder.Services.AddTransient<ISongService, SongService>();
+
+// Repositories
+builder.Services.AddTransient<IAlbumRepository, AlbumRepository>();
+builder.Services.AddTransient<IArtistRepository, ArtistRepository>();
+builder.Services.AddTransient<IReviewRepository, ReviewRepository>();
+builder.Services.AddTransient<ISongRepository, SongRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MusicDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.UseSqlite(builder.Configuration.GetConnectionString("MusicDbConnectionString"),
+        b => b.MigrationsAssembly("MusicCaCaly.WebApi"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
